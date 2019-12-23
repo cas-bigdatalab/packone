44e7e87748bd300bea48170808f40fcb1d39cb29
excelwang <wanghuajin999@163.com>
2019-12-23 13:43:55 +0800

    fix clouds reverse name

--- pk1/user/urls.py
+++ pk1/user/urls.py
@@ -20,7 +20,7 @@ def yunda_login(request):
     else:
         user, created = User.objects.get_or_create(username='yunda', is_staff=True)
     auth_login(request, user)
-    return HttpResponseRedirect(reverse('clouds'))
+    return HttpResponseRedirect(reverse('cloud'))
 
 urlpatterns = [
     re_path(r'^', include(ROUTER.urls)),
